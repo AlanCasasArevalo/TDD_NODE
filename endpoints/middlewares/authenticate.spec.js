5ef5687975c104f42ce177e7a3fab30560b41eee
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-03-19 20:03:14 +0100

    [FEATURE][SERVER] Se crean los test los cuales fallan

--- endpoints/middlewares/authenticate.spec.js
+++ endpoints/middlewares/authenticate.spec.js
@@ -1,12 +1,21 @@
 const authenticate = require('./authenticate');
 describe('MIDDLEWARES', () => {
     describe('Authentication', () => {
-        it.skip('should have id 1', () => {
-            const req = {};
-            const res = {};
+        it('should have id 1', () => {
+            const req = {
+                // El retorno del valor 1 indica que el usuario es administrador en este ejemplo
+                headers: jest.fn().mockReturnValue(1)
+            };
+            const res = {
+                sendStatus: jest.fn()
+            };
             const next = jest.fn();
-
             authenticate(req, res, next)
+            espect(req.header.mock.calls).toEqual([
+                ['user_id']
+            ])
+            espect(res.sendStatus.mock.calls).toEqual([[]])
+            espect(next.mock.calls).toEqual([[]])
         })
     })
 });
