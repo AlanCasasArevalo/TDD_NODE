68df2db8549b4106805f9f1188cafcc6f8fffe4e
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-03-19 20:19:07 +0100

    [FEATURE][SERVER] Se crea el test para que falle si el usuario no es un usuario admin

--- endpoints/middlewares/authenticate.spec.js
+++ endpoints/middlewares/authenticate.spec.js
@@ -16,6 +16,23 @@ describe('MIDDLEWARES', () => {
             expect(res.sendStatus.mock.calls).toEqual([]);
             expect(next.mock.calls).toEqual([[]])
         })
+        it('should fail if user is not one with id 1', () => {
+            const req = {
+                // El retorno del valor 1 indica que el usuario es administrador en este ejemplo
+                header: jest.fn().mockReturnValue(2)
+            };
+            const res = {
+                sendStatus: jest.fn()
+            };
+            const next = jest.fn();
+            authenticate(req, res, next)
+            expect(req.header.mock.calls).toEqual([['user_id']]);
+            expect(res.sendStatus.mock.calls).toEqual([[
+                403
+            ]]);
+            expect(next.mock.calls).toEqual([[]])
+        })
+
     })
 });
 
